182a0cfd1ea321ebdeb42fbe2036fab9c0680aef
Alistair Singh <alistair.singh7@gmail.com>
2016-05-25 20:30:41 +0200

    refactored watcher

--- wwatch.cc
+++ wwatch.cc
@@ -1,6 +1,5 @@
 
 #include <Windows.h>
-#include <cstdint>
 #include <iostream>
 #include <memory>
 #include <string>
@@ -18,20 +17,80 @@ void handle(size_t global, size_t local, unsigned action, const wstring &name,
     auto fullCommand = (command + L" " + name);
     wcout << "#######" << global << ":" << local << ":(" << hex << action
           << ") " << name << " - " << fullCommand << endl;
-    _flushall();
-    _wsystem(fullCommand.data());
+    ::_flushall();
+    ::_wsystem(fullCommand.data());
   }
 }
 
+namespace wwatch {
+class Win32Error {
+  unsigned code_;
+  std::wstring message_;
+
+public:
+  Win32Error() : code_(0) {}
+  Win32Error(unsigned code, const std::wstring &message)
+      : code_(code), message_(message) {}
+  const std::wstring &message() const { return message_; }
+  const unsigned &code() const { return code_; }
+  static Win32Error GetLastWin32Error() {
+    const unsigned size = 512;
+    Win32Error error;
+    error.code_ = ::GetLastError();
+    error.message_.reserve(size);
+    ::FormatMessageW(FORMAT_MESSAGE_FROM_SYSTEM, NULL, error.code_,
+                     MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+                     error.message_.data(), size, NULL);
+    return error;
+  }
+};
+
+class FileWatcher {
+  HANDLE directory_;
+  std::unique_ptr<char> buffer_;
+  const size_t bufferSize_;
+  const size_t iterations_;
+
+public:
+  FileWatcher(const std::wstring &path, size_t bufferSize = 4096)
+      : bufferSize_(bufferSize), iterations_(0) {
+    buffer_ = unique_ptr<char>(new char[bufferSize_]);
+    directory_ =
+        ::CreateFileW(path.data(), FILE_LIST_DIRECTORY,
+                      FILE_SHARE_WRITE | FILE_SHARE_READ | FILE_SHARE_DELETE,
+                      NULL, OPEN_EXISTING, NULL, NULL);
+    if (directory_ == INVALID_HANDLE_VALUE) {
+      throw Win32Error::GetLastWin32Error();
+    }
+  }
+  template <class THandler> unsigned poll(THandler handler) {
+    ::memset(buffer_.get(), 0, bufferSize_);
+    unsigned dwBytesReturned = 0;
+    if (::ReadDirectoryChangesW(hDir, static_cast<LPVOID>(buffer), bufferSize,
+                                FALSE, FILE_NOTIFY_CHANGE_SIZE,
+                                &dwBytesReturned, NULL, NULL) == FALSE) {
+      throw Win32Error::GetLastWin32Error();
+    } else {
+      PFILE_NOTIFY_INFORMATION notifications =
+          reinterpret_cast<PFILE_NOTIFY_INFORMATION>(buffer);
+      size_t localIteration = 0;
+      do {
+        wstring changedPath(notifications->FileName,
+                            notifications->FileNameLength / 2);
+        handler(++iterations_, ++localIteration, notifications->Action,
+                changedPath);
+        notifications += notifications->NextEntryOffset;
+      } while (notifications->NextEntryOffset != 0);
+    }
+    return dwBytesReturned;
+  }
+};
+}
+
 void printError() {
-  unsigned errorCode = GetLastError();
-  const size_t errorMessageSize = 512;
-  wchar_t errorMessage[errorMessageSize];
-  FormatMessageW(FORMAT_MESSAGE_FROM_SYSTEM, NULL, errorCode,
-                 MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), errorMessage,
-                 errorMessageSize, NULL);
-  wcerr << "Error Code: " << errorCode << endl;
-  wcerr << "Error Message: " << errorMessage << endl;
+  auto error = wwatch::Win32Error::GetLastWin32Error();
+  wcerr << "Error Code: " << error.code() << endl;
+  wcerr << "Error Message: " << error.message() << endl;
 }
 
 wstring joinCommand(int argc, wchar_t **argv) {
