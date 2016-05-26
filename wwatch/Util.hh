1299e51fec67679a2345e473a7cc53c871e193b7
Alistair Singh <alistair.singh7@gmail.com>
2016-05-26 22:33:14 +0200

    added utils

---
+++ wwatch/Util.hh
@@ -0,0 +1,37 @@
+
+#include <string>
+#include <vector>
+
+namespace wwatch {
+
+std::wstring concat(const std::vector<std::wstring> &args) {
+  std::wstring result;
+  for (const auto &arg : args) {
+    result += L" ";
+    result += arg;
+  }
+  return result;
+}
+
+bool endsWith(const std::wstring &str, const std::wstring &suffix) {
+  return str.size() >= suffix.size() &&
+         str.compare(str.size() - suffix.size(), suffix.size(), suffix) == 0;
+}
+
+std::vector<std::wstring> split(std::wstring str, wchar_t delimiter) {
+  std::vector<std::wstring> result;
+  std::wstring current;
+  for (auto const &c : str) {
+    if (c == delimiter) {
+      result.push_back(current);
+      current.clear();
+      continue;
+    }
+    current += c;
+  }
+  if (current != L"")
+    result.push_back(current);
+  return result;
+}
+}
+
