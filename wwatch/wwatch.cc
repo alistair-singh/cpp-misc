ff9ab61d7beef1b918574f1cf9ff96a7e6acd29c
Alistair Singh <alistair.singh7@gmail.com>
2016-05-26 22:32:58 +0200

    added utils

--- wwatch/wwatch.cc
+++ wwatch/wwatch.cc
@@ -1,47 +1,12 @@
 
 #include "FileWatcher.hh"
 #include "Win32Error.hh"
+#include "Util.hh"
 
-#include <Windows.h>
 #include <algorithm>
 #include <iostream>
-#include <memory>
-#include <numeric>
 #include <string>
 #include <vector>
-
-namespace wwatch {
-
-std::wstring concat(const std::vector<std::wstring> &args) {
-  std::wstring result;
-  for (const auto &arg : args) {
-    result += L" ";
-    result += arg;
-  }
-  return result;
-}
-
-bool endsWith(const std::wstring &str, const std::wstring &suffix) {
-  return str.size() >= suffix.size() &&
-         str.compare(str.size() - suffix.size(), suffix.size(), suffix) == 0;
-}
-
-std::vector<std::wstring> split(std::wstring str, wchar_t delimiter) {
-  std::vector<std::wstring> result;
-  std::wstring current;
-  for (auto const &c : str) {
-    if (c == delimiter) {
-      result.push_back(current);
-      current.clear();
-      continue;
-    }
-    current += c;
-  }
-  if (current != L"")
-    result.push_back(current);
-  return result;
-}
-}
 
 void handle(size_t global, size_t local, unsigned action,
             const std::wstring &name, const std::wstring &command,
