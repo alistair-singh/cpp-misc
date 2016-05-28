c35022ca073fad332cdacf2c206b8ca6acc4e011
Alistair Singh <alistair.singh7@gmail.com>
2016-05-28 15:59:33 +0200

    28/05/2016

--- vscpptoolspre.bat
+++ vscpptoolspre.bat
@@ -1,9 +1,13 @@
 @echo off
 
-set NUGET=%~dp0.nuget\nuget.exe
+set NUGET_DIR=%~dp0.nuget
+set NUGET=%NUGET_DIR%\nuget.exe
 set PACKAGEDIR=%~dp0packages\
 set VSTOOLSDIR=%PACKAGEDIR%VisualCppTools
 
+if "%1"=="skip" (goto :SkipDownload)
+
+mkdir %NUGET_DIR%
 if not exist %NUGET% (powershell -Command "wget http://nuget.org/nuget.exe -OutFile %NUGET%")
 
 ::if not exist %VSTOOLSDIR%* ( 
@@ -11,6 +15,10 @@ if not exist %NUGET% (powershell -Command "wget http://nuget.org/nuget.exe -OutF
 %NUGET% install VisualCppTools -SolutionDirectory . -ConfigFile %~dp0.nuget\nuget.config -NonInteractive -PreRelease -OutputDirectory %PACKAGEDIR% 
 
 ::)
+
+:SkipDownload
+
+echo %VSTOOLSDIR%
 
 for /D %%i in ( %VSTOOLSDIR%* ) do ( set NVCHOME=%%i)
 
@@ -20,6 +28,7 @@ set INCLUDE=%NVCHOME%\lib\native\include;C:\Program Files (x86)\Microsoft Visual
 set LIB=%NVCHOME%\lib\native\lib\amd64;C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\ATLMFC\LIB\amd64;C:\Program Files (x86)\Windows Kits\10\lib\10.0.10586.0\ucrt\x64;C:\Program Files (x86)\Windows Kits\NETFXSDK\4.6.1\lib\um\x64;C:\Program Files (x86)\Windows Kits\10\lib\10.0.10586.0\um\x64;
 set PATH=%NVCHOME%\lib\native\bin\amd64;%PATH%
 
+set NUGET_DIR=
 set NUGET=
 set VSTOOLSDIR=
 set PACKAGEDIR=
