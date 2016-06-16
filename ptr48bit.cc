622ea101d5e8fe0474906c8fc684c9a0c15fcb81
Alistair Singh <alistair.singh7@gmail.com>
2016-06-16 23:07:54 +0200

    added link time optimization

---
+++ ptr48bit.cc
@@ -0,0 +1,18 @@
+
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+int main() {
+  const char someStuff[] = "Some string stuff.";
+  const int size = 123;
+
+  char *str = static_cast<char *>(malloc(size));
+  printf("before %016zx\n", reinterpret_cast<size_t>(str));
+  str = reinterpret_cast<char*>(reinterpret_cast<size_t>(str) | 0x0FFFF00000000000);
+  printf("after %016zx\n", reinterpret_cast<size_t>(str));
+  memcpy(str, someStuff, sizeof(someStuff));
+  puts(str);
+  free(str);
+  return 0;
+}
