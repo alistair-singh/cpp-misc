5dde14e2167ad537ef874ac99b96c63fd1d7aea1
Alistair Singh <alistair.singh7@gmail.com>
2016-07-30 16:03:12 +0200

    changed scripts for arm

--- armv7/clang++.sh
+++ armv7/clang++.sh
@@ -1,1 +1,6 @@
-clang++ $1 -Wall -Wpedantic -Werror -O2 -std=c++11 -o $1.exe
+
+clang++ $1 -o $1.exe -Wextra -Wall -Werror -Wpedantic -std=c++14 -O2 -g
+objcopy --only-keep-debug $1.exe $1.debug
+strip -g $1.exe
+objcopy --add-gnu-debuglink=$1.debug $1.exe
+
--- armv7/g++.sh
+++ armv7/g++.sh
@@ -1,1 +1,5 @@
-g++ $1 -O2 -flto -Wall -Werror -Wpedantic -std=c++11 -o $1.exe
+g++ $1 -o $1.exe -Wextra -Wall -Werror -Wpedantic -std=c++14 -O2 -g
+objcopy --only-keep-debug $1.exe $1.debug
+strip -g $1.exe
+objcopy --add-gnu-debuglink=$1.debug $1.exe
+
