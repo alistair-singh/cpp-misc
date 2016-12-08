a82e203f164286ea38a1ffd0534e2213299397c2
Alistair Singh <alistair.singh7@gmail.com>
2016-12-08 08:25:19 +0200

    added arm

---
+++ arm-clang++.sh
@@ -0,0 +1,6 @@
+
+clang++ $1 -o $1.exe -Wextra -Wall -Werror -Wpedantic -std=c++14 -O2 -g #-flto
+#objcopy --only-keep-debug $1.exe $1.debug
+#strip -g $1.exe
+#objcopy --add-gnu-debuglink=$1.debug $1.exe
+
