05e22fe21f221dfa3ce4906197095cfc6634816e
Alistair Singh <alistair.singh7@gmail.com>
2017-03-29 21:45:07 +0200

    added cmake test

--- journal.cc
+++ journal.cc
@@ -1,6 +1,22 @@
 
 #include <iostream>
 
+struct buffer {
+  const int size = 1024*8; // 8kb
+  char data[size];
+};
+
+enum class type {
+  I32,
+  STR
+};
+
+typedef int i32;
+struct str {
+  unsigned short size;
+  char data[1];
+};
+
 int main() {
   std::cout << __FILE__ << std::endl;
   return 0;
